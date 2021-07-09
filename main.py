a6941e914c864477e72b1903b6330303871c84af
SashaVM <sashavictoriam@icloud.com>
2021-07-09 09:26:27 -0700

    verify doctor name + id and login

---
+++ main.py
@@ -0,0 +1,38 @@
+# To start:
+# export FLASK_APP=main.py
+# export FLASK_ENV=development
+# flask run
+
+from flask import Flask, request, render_template
+app = Flask(__name__)
+import queries
+
+@app.route("/", methods=['GET'])
+def login():
+    message = "Please log in to view data"
+    return render_template('login.html', message=message)
+
+
+@app.route("/home/<userType>", methods=['POST'])
+def main(userType):
+    message = "Welcome!"
+    if(userType == "guest"):
+        return render_template('home.html', userName="Guest", message=message)
+    elif(userType == "registered"):
+        name = request.form.get('lastName')
+        userID = int(request.form.get('doctor_id'))
+        userData = verifyLogin(name, userID)
+        if(userData == None):
+            return render_template('login.html', message="User data not found")
+        return render_template('home.html', userName=(userData[1]), message=message)
+
+
+def verifyLogin(doctorName, doctorID):
+    doctorData = queries.findDataMatch("doctors", "lastName", doctorName, True)
+    if (len(doctorData) == 0):
+        return None
+    else:
+        for row in doctorData:
+            if((row[0] == doctorID) and (row[2] == doctorName)):
+                return row
+        return None
---
+++ queries.py
@@ -0,0 +1,53 @@
+import secret
+import pymysql
+db = pymysql.connect(
+    host="freetrainer.cryiqqx3x1ub.us-west-2.rds.amazonaws.com",
+    user="sasha",
+    password=secret.password
+)
+cursor = db.cursor()
+
+def findDataMatch(table, column, searchText, exactMatch):
+    if(exactMatch):
+        sql = f"SELECT * FROM sasha_mackowiak.{table} WHERE {column}='{searchText}'"
+    else:
+        sql = f"SELECT * FROM sasha_mackowiak.{table} WHERE {column}='%{searchText}%'"
+    return getRows(sql)
+
+
+def getRows(script):
+    result = []
+    cursor.execute(script)
+    while(True):
+        row = cursor.fetchone()
+        if(row == None):
+            break
+        result.append(row)
+    db.close
+    return result
+
+
+# cursor.execute("SELECT * FROM sasha_mackowiak.quantum_powers")
+# while(True):
+#     row = cursor.fetchone()
+#     if row == None:
+#         break;
+#     print(row)
+# db.close()
+
+
+# def printRows(table, limit):
+#     sql = f'SELECT * FROM sasha_mackowiak.{table} LIMIT {limit}'
+#     cursor.execute(sql)
+#     while(True):
+#         row = cursor.fetchone()
+#         if row == None:
+#             break;
+#         for cell in row:
+#             print(cell)
+#     db.close()
+
+
+# tableName = input("Which table?")
+# rows = int(input("How many rows?"))
+# printRows(tableName, rows)
